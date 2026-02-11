1fc080b2aa54675d9ee857ace0e04e3d88694583
dmorris <DanRhysMorris@gmail.com>
2026-02-11 23:55:35 +0000

    Add Platform.hpp for macros defining cross-platform constants and symbols

---
+++ src/Strawberry/Net/Socket/Platform.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+
+// Define the type of the return value of socket functions.
+#if STRAWBERRY_TARGET_WINDOWS
+#define SOCKET_ERROR_CODE_TYPE decltype(SOCKET_ERROR)
+#elif STRAWBERRY_TARGET_MAC || STRAWBERRY_TARGET_LINUX
+#define SOCKET_ERROR_CODE_TYPE int
+#endif
+
+
+// Define macro for the value which socket function return on an error.
+#if STRAWBERRY_TARGET_WINDOWS
+#define SOCKET_ERROR_CODE SOCKET_ERROR
+#elif STRAWBERRY_TARGET_MAC || STRAWBERRY_TARGET_LINUX
+#define SOCKET_ERROR_CODE -1
+#endif
+
+
+// Define the platform dependant type used for socket option setting
+#if STRAWBERRY_TARGET_WINDOWS
+#define SOCKET_OPTION_TYPE DWORD
+#elif STRAWBERRY_TARGET_MAC || STRAWBERRY_TARGET_LINUX
+#define SOCKET_OPTION_TYPE int
+#endif
