546c20603e99cafd6e23880314b87915580b0798
dmorris <DanRhysMorris@gmail.com>
2026-02-11 20:43:54 +0000

    Tabified Error.hpp

--- src/Strawberry/Net/Error.hpp
+++ src/Strawberry/Net/Error.hpp
@@ -6,38 +6,38 @@
 
 namespace Strawberry::Net
 {
-    struct ErrorDNSResolution {};
-    struct ErrorSocketCreation {};
-    struct ErrorSocketBinding {};
-    struct ErrorAddressResolution {};
-    struct ErrorEstablishConnection {};
-    struct ErrorSSLAllocation {};
-    struct ErrorSSLHandshake {};
-    struct ErrorParsingEndpoint {};
-    struct ErrorParsingRTPPacket {};
-    struct ErrorConnectionReset {};
-    struct ErrorNoData {};
-    struct ErrorProtocolError {};
-    struct ErrorRefused {};
-    struct ErrorOpenSSL {};
-    struct ErrorSystem {};
+	struct ErrorDNSResolution {};
+	struct ErrorSocketCreation {};
+	struct ErrorSocketBinding {};
+	struct ErrorAddressResolution {};
+	struct ErrorEstablishConnection {};
+	struct ErrorSSLAllocation {};
+	struct ErrorSSLHandshake {};
+	struct ErrorParsingEndpoint {};
+	struct ErrorParsingRTPPacket {};
+	struct ErrorConnectionReset {};
+	struct ErrorNoData {};
+	struct ErrorProtocolError {};
+	struct ErrorRefused {};
+	struct ErrorOpenSSL {};
+	struct ErrorSystem {};
 
 
 
-    using Error = Core::Variant<
-        ErrorDNSResolution,
-        ErrorSocketCreation,
-        ErrorSocketBinding,
-        ErrorAddressResolution,
-        ErrorEstablishConnection,
-        ErrorSSLAllocation,
-        ErrorSSLHandshake,
-        ErrorParsingEndpoint,
-        ErrorParsingRTPPacket,
-        ErrorConnectionReset,
-        ErrorNoData,
-        ErrorProtocolError,
-        ErrorRefused,
-        ErrorOpenSSL,
-        ErrorSystem>;
+	using Error = Core::Variant<
+		ErrorDNSResolution,
+		ErrorSocketCreation,
+		ErrorSocketBinding,
+		ErrorAddressResolution,
+		ErrorEstablishConnection,
+		ErrorSSLAllocation,
+		ErrorSSLHandshake,
+		ErrorParsingEndpoint,
+		ErrorParsingRTPPacket,
+		ErrorConnectionReset,
+		ErrorNoData,
+		ErrorProtocolError,
+		ErrorRefused,
+		ErrorOpenSSL,
+		ErrorSystem>;
 }
