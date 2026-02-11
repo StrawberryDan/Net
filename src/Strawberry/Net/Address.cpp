df63f2fabe0959271a01bc4d0415bb3ea2c0ca67
dmorris <DanRhysMorris@gmail.com>
2026-02-11 16:22:00 +0000

    Reformat Address.cpp

--- src/Strawberry/Net/Address.cpp
+++ src/Strawberry/Net/Address.cpp
@@ -1,18 +1,12 @@
+// Strawberry dnet
 #include "Strawberry/Net/Address.hpp"
-
-
+// Strawberry Core
 #include "Strawberry/Core/Assert.hpp"
 #include "Strawberry/Core/Markers.hpp"
-#include <numeric>
-
-
+// OS-Level Networkng Headers
 #if STRAWBERRY_TARGET_MAC || STRAWBERRY_TARGET_LINUX
-
-
 #include <arpa/inet.h>
 #include <netdb.h>
-
-
 #elif STRAWBERRY_TARGET_WINDOWS
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -21,122 +15,122 @@
 
 namespace Strawberry::Net
 {
-    Core::Optional<IPv4Address> IPv4Address::Parse(const std::string& data)
-    {
-        uint8_t buffer[sizeof(in_addr)] = {0};
-        auto    result                  = inet_pton(AF_INET, data.data(), buffer);
-        if (result == 1)
-        {
-            Core::IO::ByteBuffer<4> bytes(buffer, 4);
-            return IPv4Address(bytes);
-        }
-        else
-        {
-            return {};
-        }
-    }
+	Core::Optional<IPv4Address> IPv4Address::Parse(const std::string& data)
+	{
+		uint8_t buffer[sizeof(in_addr)] = {0};
+		auto	result					= inet_pton(AF_INET, data.data(), buffer);
+		if (result == 1)
+		{
+			Core::IO::ByteBuffer<4> bytes(buffer, 4);
+			return IPv4Address(bytes);
+		}
+		else
+		{
+			return {};
+		}
+	}
 
 
-    Core::IO::ByteBuffer<4> IPv4Address::AsBytes() const
-    {
-        return mData;
-    }
+	Core::IO::ByteBuffer<4> IPv4Address::AsBytes() const
+	{
+		return mData;
+	}
 
 
-    std::string IPv4Address::AsString() const
-    {
-        char buffer[INET_ADDRSTRLEN] = {0};
-        auto result                  = inet_ntop(AF_INET, mData.Data(), buffer, INET_ADDRSTRLEN);
-        Core::Assert(result != nullptr);
-        return {buffer};
-    }
+	std::string IPv4Address::AsString() const
+	{
+		char buffer[INET_ADDRSTRLEN] = {0};
+		auto result					 = inet_ntop(AF_INET, mData.Data(), buffer, INET_ADDRSTRLEN);
+		Core::Assert(result != nullptr);
+		return {buffer};
+	}
 
 
-    Core::Optional<IPv6Address> IPv6Address::Parse(const std::string& string)
-    {
-        uint8_t buffer[sizeof(in6_addr)] = {0};
-        auto    result                   = inet_pton(AF_INET6, string.data(), buffer);
-        if (result == 1)
-        {
-            Core::IO::ByteBuffer<16> bytes(buffer, 16);
-            return IPv6Address(bytes);
-        }
-        else
-        {
-            return {};
-        }
-    }
+	Core::Optional<IPv6Address> IPv6Address::Parse(const std::string& string)
+	{
+		uint8_t buffer[sizeof(in6_addr)] = {0};
+		auto	result					 = inet_pton(AF_INET6, string.data(), buffer);
+		if (result == 1)
+		{
+			Core::IO::ByteBuffer<16> bytes(buffer, 16);
+			return IPv6Address(bytes);
+		}
+		else
+		{
+			return {};
+		}
+	}
 
 
-    const Core::IO::ByteBuffer<16>& IPv6Address::AsBytes() const
-    {
-        return mData;
-    }
+	const Core::IO::ByteBuffer<16>& IPv6Address::AsBytes() const
+	{
+		return mData;
+	}
 
 
-    std::string IPv6Address::AsString() const
-    {
-        char buffer[INET6_ADDRSTRLEN] = {0};
-        auto result                   = inet_ntop(AF_INET6, mData.Data(), buffer, INET6_ADDRSTRLEN);
-        Core::Assert(result != nullptr);
-        return {buffer};
-    }
+	std::string IPv6Address::AsString() const
+	{
+		char buffer[INET6_ADDRSTRLEN] = {0};
+		auto result					  = inet_ntop(AF_INET6, mData.Data(), buffer, INET6_ADDRSTRLEN);
+		Core::Assert(result != nullptr);
+		return {buffer};
+	}
 
 
-    IPAddress::IPAddress(IPv4Address address)
-        : mPayload(address) {}
+	IPAddress::IPAddress(IPv4Address address)
+		: mPayload(address) {}
 
 
-    IPAddress::IPAddress(IPv6Address address)
-        : mPayload(address) {}
+	IPAddress::IPAddress(IPv6Address address)
+		: mPayload(address) {}
 
 
-    Core::Optional<IPv4Address> IPAddress::AsIPv4() const
-    {
-        if (IsIPv4()) return mPayload.Ref<IPv4Address>();
+	Core::Optional<IPv4Address> IPAddress::AsIPv4() const
+	{
+		if (IsIPv4()) return mPayload.Ref<IPv4Address>();
 
-        return {};
-    }
-
-
-    Core::Optional<IPv6Address> IPAddress::AsIPv6() const
-    {
-        if (IsIPv6()) return mPayload.Ref<IPv6Address>();
-
-        return {};
-    }
+		return {};
+	}
 
 
-    Core::IO::DynamicByteBuffer IPAddress::AsBytes() const
-    {
-        if (auto addr = AsIPv4())
-        {
-            return Core::IO::DynamicByteBuffer(addr->AsBytes());
-        }
-        else if (auto addr = AsIPv6())
-        {
-            return Core::IO::DynamicByteBuffer(addr->AsBytes());
-        }
-        else
-        {
-            Core::Unreachable();
-        }
-    }
+	Core::Optional<IPv6Address> IPAddress::AsIPv6() const
+	{
+		if (IsIPv6()) return mPayload.Ref<IPv6Address>();
+
+		return {};
+	}
 
 
-    std::string IPAddress::AsString() const
-    {
-        if (auto addr = AsIPv4())
-        {
-            return addr->AsString();
-        }
-        else if (auto addr = AsIPv6())
-        {
-            return addr->AsString();
-        }
-        else
-        {
-            Core::Unreachable();
-        }
-    }
+	Core::IO::DynamicByteBuffer IPAddress::AsBytes() const
+	{
+		if (auto addr = AsIPv4())
+		{
+			return Core::IO::DynamicByteBuffer(addr->AsBytes());
+		}
+		else if (auto addr = AsIPv6())
+		{
+			return Core::IO::DynamicByteBuffer(addr->AsBytes());
+		}
+		else
+		{
+			Core::Unreachable();
+		}
+	}
+
+
+	std::string IPAddress::AsString() const
+	{
+		if (auto addr = AsIPv4())
+		{
+			return addr->AsString();
+		}
+		else if (auto addr = AsIPv6())
+		{
+			return addr->AsString();
+		}
+		else
+		{
+			Core::Unreachable();
+		}
+	}
 } // namespace Strawberry::Net
