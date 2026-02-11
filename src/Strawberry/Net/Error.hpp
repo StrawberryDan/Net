9f0de38e055a45771e59660fcce9193ffd814c65
dmorris <DanRhysMorris@gmail.com>
2026-02-11 20:43:16 +0000

    Add error type for socket binding

--- src/Strawberry/Net/Error.hpp
+++ src/Strawberry/Net/Error.hpp
@@ -8,6 +8,7 @@ namespace Strawberry::Net
 {
     struct ErrorDNSResolution {};
     struct ErrorSocketCreation {};
+    struct ErrorSocketBinding {};
     struct ErrorAddressResolution {};
     struct ErrorEstablishConnection {};
     struct ErrorSSLAllocation {};
@@ -26,6 +27,7 @@ namespace Strawberry::Net
     using Error = Core::Variant<
         ErrorDNSResolution,
         ErrorSocketCreation,
+        ErrorSocketBinding,
         ErrorAddressResolution,
         ErrorEstablishConnection,
         ErrorSSLAllocation,
