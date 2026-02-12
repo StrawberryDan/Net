9397afd1bb6b7575f94e6f099d36d2866b1d390b
dmorris <DanRhysMorris@gmail.com>
2026-02-12 00:02:38 +0000

    Add proper testing for TCPSockets

---
+++ test/TCP.cpp
@@ -0,0 +1,71 @@
+#include "Strawberry/Core/Assert.hpp"
+#include "Strawberry/Core/IO/DynamicByteBuffer.hpp"
+#include "Strawberry/Net/Address.hpp"
+#include "Strawberry/Net/Endpoint.hpp"
+#include "Strawberry/Net/Socket/TCPListener.hpp"
+#include "Strawberry/Net/Socket/TCPSocket.hpp"
+#include <chrono>
+#include <random>
+#include <thread>
+
+using namespace Strawberry;
+using namespace Net;
+
+
+int main()
+{
+	static constexpr size_t MESSAGE_SIZE = 1024 * 1024;
+	std::random_device rng;
+
+
+	std::array endpoints
+		{
+			Endpoint(IPv4Address::LocalHost(), 65535 - 1000),
+			Endpoint(IPv6Address::LocalHost(), 65535 - 1001)
+		};
+
+
+	for (auto endpoint : endpoints)
+	{
+		// Create listener.
+		Core::Logging::Trace("Opening listener!");
+		auto listener = Socket::TCPListener::Bind(endpoint).Unwrap();
+		// Create client
+		Core::Logging::Trace("Opening client");
+		auto client = Socket::TCPSocket::Connect(endpoint).Unwrap();
+
+		// Accept client from listener.
+		auto listenerSocket = listener.Accept().Unwrap();
+		Core::Logging::Trace("Accepted client");
+
+		// Generate random message.
+		Core::IO::DynamicByteBuffer message;
+		for (int i = 0; i < MESSAGE_SIZE; i++)
+		{
+			message.Push<uint8_t>(rng());
+		}
+
+		// Send message to server
+		Core::Logging::Trace("Sending message from client");
+		client.Write(message).Unwrap();
+
+		// Wait for a second.
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+
+		// Read message from listenerSocket.
+		Core::Logging::Trace("Reading message");
+		auto receivedMessage = listenerSocket.Read(MESSAGE_SIZE).Unwrap();
+		Core::AssertEQ(receivedMessage, message);
+
+		message.Clear();
+		for (int i = 0; i < MESSAGE_SIZE; i++)
+		{
+			message.Push<uint8_t>(rng());
+		}
+
+		listenerSocket.Write(message).Unwrap();
+
+		receivedMessage = client.Read(MESSAGE_SIZE).Unwrap();
+		Core::AssertEQ(receivedMessage, message);
+	}
+}
