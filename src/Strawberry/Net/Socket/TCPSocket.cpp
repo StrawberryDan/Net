edfbef409256e30baf3aaa26672f3eaffb5e4fab
dmorris <DanRhysMorris@gmail.com>
2026-02-12 18:25:52 +0000

    Refactor and add logging to TCPSocket

--- src/Strawberry/Net/Socket/TCPSocket.cpp
+++ src/Strawberry/Net/Socket/TCPSocket.cpp
@@ -23,45 +23,53 @@ namespace Strawberry::Net::Socket
 {
 	Core::Result<TCPSocket, Error> TCPSocket::Connect(const Endpoint& endpoint)
 	{
+		Core::Logging::Info("Connecting TCP Socket to {}", endpoint.ToString());
+
+
 		TCPSocket tcpSocket;
 
 		addrinfo hints{.ai_flags = AI_ADDRCONFIG, .ai_socktype = SOCK_STREAM, .ai_protocol = IPPROTO_TCP};
-		hints.ai_family = endpoint.GetAddress()->IsIPv4() ? AF_INET : endpoint.GetAddress()->IsIPv6() ? AF_INET6 : Core::Unreachable<int>();
+		hints.ai_family = endpoint.GetAddress()->IsIPv4() ? AF_INET
+			: endpoint.GetAddress()->IsIPv6() ? AF_INET6
+			: Core::Unreachable<int>();
 
 		addrinfo* peerAddress = nullptr;
 		auto	  addrResult  = getaddrinfo(endpoint.GetAddress()->AsString().c_str(), std::to_string(endpoint.GetPort()).c_str(), &hints, &peerAddress);
 		if (addrResult != 0)
-			{
-				freeaddrinfo(peerAddress);
-				return ErrorAddressResolution {};
-			}
+		{
+			Core::Logging::Error("Failed to resolve address information for endpoint: {}", endpoint.ToString());
+			freeaddrinfo(peerAddress);
+			return ErrorAddressResolution {};
+		}
 
 		auto handle = socket(peerAddress->ai_family, peerAddress->ai_socktype, peerAddress->ai_protocol);
 		if (handle == -1)
-			{
-				freeaddrinfo(peerAddress);
-				return ErrorSocketCreation {};
-			}
+		{
+			Core::Logging::Error("Failed to create TCP Socket for endpoint {}", endpoint.ToString());
+			freeaddrinfo(peerAddress);
+			return ErrorSocketCreation {};
+		}
 
 		auto connection = connect(handle, peerAddress->ai_addr, peerAddress->ai_addrlen);
 		if (connection == -1)
-			{
-				freeaddrinfo(peerAddress);
-				return ErrorAddressResolution {};
-			}
+		{
+			Core::Logging::Error("Failed to estabilish TCP connection to endpoint {}", endpoint.ToString());
+			freeaddrinfo(peerAddress);
+			return ErrorAddressResolution {};
+		}
 
 		freeaddrinfo(peerAddress);
 		tcpSocket.mSocket	= handle;
 		tcpSocket.mEndpoint = endpoint;
 
-#if STRAWBERRY_TARGET_WINDOWS
-		DWORD keepAlive = 1;
-#elif STRAWBERRY_TARGET_MAC || STRAWBERRY_TARGET_LINUX
-		int keepAlive = 1;
-#endif
-		Core::AssertEQ(
-					   setsockopt(tcpSocket.mSocket, SOL_SOCKET, SO_KEEPALIVE, reinterpret_cast<const char*>(&keepAlive), sizeof(keepAlive)),
-					   0);
+		SOCKET_OPTION_TYPE keepAlive = 1;
+		SOCKET_ERROR_CODE_TYPE optResult =
+			setsockopt(tcpSocket.mSocket, SOL_SOCKET, SO_KEEPALIVE,
+					   reinterpret_cast<const char*>(&keepAlive), sizeof(keepAlive));
+		Core::AssertEQ(optResult, 0);
+
+
+		Core::Logging::Info("Connected TCP Socket ({}) to {}", tcpSocket.mSocket, endpoint.ToString());
 		return tcpSocket;
 	}
 
@@ -78,10 +86,10 @@ namespace Strawberry::Net::Socket
 	TCPSocket& TCPSocket::operator=(TCPSocket&& other) noexcept
 	{
 		if (this != &other)
-			{
-				std::destroy_at(this);
-				std::construct_at(this, std::move(other));
-			}
+		{
+			std::destroy_at(this);
+			std::construct_at(this, std::move(other));
+		}
 
 		return *this;
 	}
@@ -90,15 +98,10 @@ namespace Strawberry::Net::Socket
 	TCPSocket::~TCPSocket()
 	{
 		if (mSocket != -1)
-			{
-#if STRAWBERRY_TARGET_MAC || STRAWBERRY_TARGET_LINUX
-				close(mSocket);
-#elif STRAWBERRY_TARGET_WINDOWS
-				closesocket(mSocket);
-#else
-				Core::Unreachable();
-#endif
-			}
+		{
+			Core::Logging::Info("Closing socket ({})", mSocket);
+			CLOSE_SOCKET_FUNCTION(mSocket);
+		}
 	}
 
 
@@ -110,34 +113,18 @@ namespace Strawberry::Net::Socket
 
 	bool TCPSocket::Poll() const
 	{
-#if STRAWBERRY_TARGET_MAC || STRAWBERRY_TARGET_LINUX
-		pollfd fds[] = {
-			{mSocket, POLLIN, 0}
+		SOCKET_POLL_FD_TYPE fds[] = {
+			{ mSocket, POLLIN, 0}
 		};
 
-		int pollResult = poll(fds, 1, 0);
-		Core::Assert(pollResult >= 0);
+		int pollResult = SOCKET_POLL_FUNCTION(fds, 1, 0);
+		if (pollResult <= 0)
+		{
+			Core::Logging::Error("Error when polling TCP socket! Error code: {}", API::GetError());
+			return false;
+		}
+
 		return static_cast<bool>(fds[0].revents & POLLIN);
-#elif STRAWBERRY_TARGET_WINDOWS
-		WSAPOLLFD fds[] =
-			{
-				{mSocket, POLLIN, 0}
-			};
-		ErrorCode pollResult = WSAPoll(fds, 1, 0);
-		if (pollResult == -1)
-			{
-				ErrorCode error = API::GetError();
-				switch (error)
-					{
-					default: Core::Logging::Error("Unknown error on TCP recv: {}", error);
-						Core::Unreachable();
-					}
-			}
-		Core::Assert(pollResult >= 0);
-		return static_cast<bool>(fds[0].revents & POLLIN);
-#else
-		Core::Unreachable();
-#endif
 	}
 
 
@@ -146,19 +133,16 @@ namespace Strawberry::Net::Socket
 		auto buffer = Core::IO::DynamicByteBuffer::Zeroes(length);
 
 		auto recvResult = recv(mSocket, reinterpret_cast<char*>(buffer.Data()), buffer.Size(), 0);
-		if (recvResult == -1)
+		if (recvResult == SOCKET_ERROR_CODE)
+		{
+			switch (auto error = API::GetError())
 			{
-				auto error = API::GetError();
-				switch (error)
-					{
-					case ErrorCodes::ConnectionReset: return Error(ErrorConnectionReset {});
-					default: Core::Logging::Error("Unknown error on TCP recv: {}", error);
-						Core::Unreachable();
-					}
+			default:
+				Core::Logging::Error("Unhandled error code in TCPSocket::Read! Code: {}.", error);
+				return ErrorUnknown{};
 			}
+		}
 
-
-		if (recvResult == 0) return Error(ErrorConnectionReset {});
 		Core::Assert(recvResult > 0);
 		buffer.Resize(recvResult);
 		return buffer;
@@ -169,20 +153,19 @@ namespace Strawberry::Net::Socket
 	{
 		auto buffer = Core::IO::DynamicByteBuffer::WithCapacity(length);
 
-
 		while (buffer.Size() < length)
-			{
-				auto read = Read(length - buffer.Size());
+		{
+			auto read = Read(length - buffer.Size());
 
-				if (read.IsOk())
-					{
-						buffer.Push(read.Unwrap());
-					}
-				else
-					{
-						return read.Err();
-					}
+			if (read.IsOk())
+			{
+				buffer.Push(read.Unwrap());
 			}
+			else
+			{
+				return read.Err();
+			}
+		}
 
 		return buffer;
 	}
@@ -193,22 +176,22 @@ namespace Strawberry::Net::Socket
 		size_t bytesSent = 0;
 
 		while (bytesSent < bytes.Size())
+		{
+			auto sendResult = send(mSocket, reinterpret_cast<const char*>(bytes.Data()) + bytesSent, bytes.Size() - bytesSent, 0);
+			if (sendResult > 0)
 			{
-				auto sendResult = send(mSocket, reinterpret_cast<const char*>(bytes.Data()) + bytesSent, bytes.Size() - bytesSent, 0);
-				if (sendResult > 0)
-					{
-						bytesSent += sendResult;
-					}
-				else
-					{
-						switch (auto error = API::GetError())
-							{
-							case ErrorCodes::ConnectionReset: return ErrorConnectionReset {};
-							default: Core::Logging::Error("Unknown error on TCP send: {}", error);
-								Core::Unreachable();
-							}
-					}
+				bytesSent += sendResult;
 			}
+			else
+			{
+				switch (auto error = API::GetError())
+				{
+				default:
+					Core::Logging::Error("Unhandled error code in TCPSocket::Write! Code: {}.", error);
+					return ErrorUnknown{};
+				}
+			}
+		}
 
 		Core::Assert(bytesSent == bytes.Size());
 		return Core::Success;
